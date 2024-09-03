1c58c5016ee47ae3a21e5d9deaaf87d9d07eb887
Cyprien Ferreira <cyferrei@paul-f4Br4s7.clusters.42paris.fr>
2024-09-03 15:33:53 +0200

    minor modifs in some functions

--- libft_cub3D/ft_strrchr.c
+++ libft_cub3D/ft_strrchr.c
@@ -6,13 +6,13 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 18:00:46 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/03 13:32:40 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/09/03 15:27:46 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_cub3D.h"
 
-char	*ft_strrchr(const char *str, int searchedChar)
+char	*ft_strrchr(char *str, int searchedChar)
 {
 	int	len;
 
