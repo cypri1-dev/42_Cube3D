246ddd270bb05439b9e08c5abb0d5983cc2af375
Cyprien Ferreira <cyferrei@paul-f4Br4s7.clusters.42paris.fr>
2024-09-03 15:33:11 +0200

    [ADD]: add new defines and prototypes

--- includes/cub3D_lib.h
+++ includes/cub3D_lib.h
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 13:20:45 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/03 13:29:14 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/09/03 15:26:48 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@
 
 /*all defines*/
 
+# define ZERO_INIT 0
+# define NULL_INIT NULL
 # define PI 3.14159265359
 # define MAP_WIDTH 6
 # define MAP_HEIGHT 5
@@ -46,5 +48,27 @@ typedef struct s_data
 	int wall;
 	int ground;
 }				t_data;
+
+/*strings functions prototypes*/
+
+int			ft_strlen(char *str);
+char		*ft_strrchr(const char *str, int searchedChar);
+int			ft_strcmp(const char *s1, const char *s2);
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+char		*ft_itoa(int nbr);
+char		*ft_strdup(const char *s);
+char		*ft_strchr(const char *s, int c);
+void		ft_putnbr_fd(int n, int fd);
+char		**ft_split(char const *s, char c);
+char		*ft_strstr(char *haystack, char *needle);
+
+/*parsing function*/
+
+void	checker(int argc, char **argv, char **envp);
+void	check_envp(char **envp);
+void	check_nb_args(int argc);
+void	check_extension(char **argv);
+int	ft_count_dot(char *argv, char c);
 
 #endif
